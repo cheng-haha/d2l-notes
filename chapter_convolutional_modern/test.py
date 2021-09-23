a0dca85dcd3d09a1342405ae939b181c9502b549
cheng-haha <54107313+cheng-haha@users.noreply.github.com>
2021-09-23 16:36:18 +0800

    Add files via upload

---
+++ chapter_convolutional_modern/test.py
@@ -0,0 +1,10 @@
+'''
+Author: your name
+Date: 2021-09-16 13:01:13
+LastEditTime: 2021-09-16 13:03:23
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: \d2l-zh\learnNotes\chapter_convolutional_modern\test.py
+'''
+import matplotlib.pyplot as plt
+camp = plt.cm.Blues
